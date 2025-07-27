2a7eb7e6d67842c75a34a46bce707c846151f006
ilyako87 <ilyako87@users.noreply.github.com>
2025-07-27 16:40:43 +0000

    deploy: 772cab050d12a5bf40adf652d24e383dd1c80f83

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/page/precache-manifest.6c433e4c60809e213f469233f5511c82.js"
+  "/page/precache-manifest.6d342cc0fd2ee03d6d307fb5d0e1f2e0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "cred0-landing-page"});
